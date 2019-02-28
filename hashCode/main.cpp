444e138966427021b32256c1e7a6737ce6859022
Mohammad Massoud <36207910+massoudsalem@users.noreply.github.com>
2019-02-28 21:22:44 +0200

    Update main.cpp

--- hashCode/main.cpp
+++ hashCode/main.cpp
@@ -1,48 +1,34 @@
 #include <bits/stdc++.h>
-#define el '\n'
-#define fornp(i, x, n) for (int i = x; i < int(n); i++)
-#define fornm(i, x, n) for (int i = x; i >= int(n); i--)
-#define rep(i,n) for (int i = 0; i < int(n); i++)
-#define MAXN (int)1e7+10
-#define vi vector<int>
-#define vll vector<ll>
-#define MOD (int)((1e9)+7)
-#define oo (int)(2e9)
-#define OO (ll)(1e18)
-#define clr(container, val) memset(container, val, sizeof(container))
-#define sz(container) container.size()
-typedef long long ll;
+
 
 using namespace std;
-class photo
-{
-public:
-    int state;
-    int noTags;
-    vector<int> tags;
 
-};
-int main()
-{
-
-    int noPhotos;
-    cin >> noPhotos;
-    photo f[noPhotos];
-    map<>
-    for(int i = 0; i <noPhotos; i++)
-    {
-        char c;
-        cin>>c>>f[i].noTags;
-        f[i].state=(c=='H')?1:0;
-        for(int j=0; j<f[i].noTags; j++)
-        {
-            string s;
-            cin>>s;
-            int x=hash(s);
-            f[i].tags.push_back(x);
-        }
+int main(){
+  int noPhotos;
+  char orientaion;
+  int noTags;
+  cin >> noPhotos;
+  photo f[noPhotos];
+  map<string, vector<pair<int,bool>> > tagContainer;
+  set <string> tags;
+  vector<int> v,h;
+  for(int i=0;i<noPhotos;++i){
+    cin>>orientaion;
+    cin>>noTags;
+    while(noTags--){
+      string tag;
+      cin>>tag;
+      tags.insert(tag);
+      tagContainer[tag].push_back({i,(orientaion=='H'?1:0)});
+      if(orientaion=='H'){
+        h.push_back(i);
+      }else{
+        v.push_back(i);
+      }
     }
+  }
+  for(int j=0;j<tags.size();++j){
+    
+  }
 
-    return 0;
 }
-
