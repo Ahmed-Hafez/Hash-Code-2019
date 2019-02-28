c9858b8b0dd03fd7fb2116346444b5f5729043ac
Ahmed Hafez <Ahmed41901@gmail.com>
2019-02-28 21:56:46 +0200

    solved

--- hashCode/main.cpp
+++ hashCode/main.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 
 int main(){
+    freopen("e_shiny_selfies.txt","r",stdin);
+    freopen("out4.txt","w",stdout);
   int noPhotos;
   char orientaion;
   int noTags;
   cin >> noPhotos;
-  photo f[noPhotos];
+
   map<string, vector<pair<int,bool>> > tagContainer;
   set <string> tags;
   vector<int> v,h;
@@ -27,8 +29,27 @@ int main(){
       }
     }
   }
-  for(int j=0;j<tags.size();++j){
-    
+  map<int,int>mp;
+  vector <int>H,V;
+  for(auto tag:tags){
+    for(auto it:tagContainer[tag]){
+            if(!mp[it.first]){
+        if(it.second==0)V.push_back(it.first);
+        else H.push_back(it.first);
+        mp[it.first]++;
+            }
+    }
   }
+cout<<V.size()/2+H.size()<<endl;
 
+for(auto i:H){
+    cout<<i<<endl;
+}
+int x=(V.size()/2)*2;
+for(int i=0;i<x;i++){
+
+    cout<<V[i];
+    if(i&1)cout<<endl;
+    else cout<<" ";
+}
 }
