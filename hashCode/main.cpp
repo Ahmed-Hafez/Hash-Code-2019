5c318e590305d35a8984346699f2974f2b64a13b
Ahmed Hafez <Ahmed41901@gmail.com>
2019-02-28 22:57:54 +0200

    7aga keda

--- hashCode/main.cpp
+++ hashCode/main.cpp
@@ -12,64 +12,70 @@ int main()
         "d_pet_pictures.txt",
         "e_shiny_selfies.txt"
         */
-        freopen("b_lovely_landscapes.txt","r",stdin);
-        freopen("out1.txt","w",stdout);
+    freopen("b_lovely_landscapes.txt","r",stdin);
+    freopen("out0.txt","w",stdout);
 
-        int noPhotos;
-        char orientaion;
-        int noTags;
-        cin >> noPhotos;
+    int noPhotos;
+    char orientaion;
+    int noTags;
+    cin >> noPhotos;
 
-        map<string, vector<pair<int,bool>> > tagContainer;
-        set <string> tags;
-        vector<int> v,h;
-        for(int i=0; i<noPhotos; ++i)
+    map<string, vector<pair<int,bool>> > tagContainer;
+    set <string> tags;
+    vector<int> v,h;
+    for(int i=0; i<noPhotos; ++i)
+    {
+        cin>>orientaion;
+        cin>>noTags;
+        while(noTags--)
         {
-            cin>>orientaion;
-            cin>>noTags;
-            while(noTags--)
+            string tag;
+            cin>>tag;
+            tags.insert(tag);
+            tagContainer[tag].push_back({i,(orientaion=='H'?1:0)});
+            if(orientaion=='H')
             {
-                string tag;
-                cin>>tag;
-                tags.insert(tag);
-                tagContainer[tag].push_back({i,(orientaion=='H'?1:0)});
-                if(orientaion=='H')
-                {
-                    h.push_back(i);
-                }
-                else
-                {
-                    v.push_back(i);
-                }
+                h.push_back(i);
+            }
+            else
+            {
+                v.push_back(i);
             }
         }
-        map<int,int>mp;
-        vector <int>H,V;
-        for(auto tag:tags)
+    }
+    map<int,int>mp;
+    vector <int>H,V;
+    for(auto tag:tags)
+    {
+        for(auto it:tagContainer[tag])
         {
-            for(auto it:tagContainer[tag])
+            if(!mp[it.first])
             {
-                if(!mp[it.first])
-                {
-                    if(it.second==0)V.push_back(it.first);
-                    else H.push_back(it.first);
-                    mp[it.first]++;
-                }
+                if(it.second==0)V.push_back(it.first);
+                else H.push_back(it.first);
+                mp[it.first]++;
             }
         }
-        cout<<V.size()/2+H.size()<<endl;
+    }
+    cout<<V.size()/2+H.size()<<endl;
+    int j=0;
+    int x=(V.size()/2)*2;
 
-        for(auto i:H)
-        {
-            cout<<i<<endl;
-        }
-        int x=(V.size()/2)*2;
-        for(int i=0; i<x; i++)
+    for(int i=0; i<H.size(); i++)
+    {
+
+       if(i%2==0) cout<<H[i]<<endl;
+
+        for(; j<x; j++)
         {
 
-            cout<<V[i];
-            if(i&1)cout<<endl;
+            cout<<V[j];
+            if(j&1){cout<<endl;
+            j++;break;
+            }
             else cout<<" ";
+
         }
 
+    }
 }
